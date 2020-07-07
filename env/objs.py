d622f3c71ee3da3808f34498fd5df2c3e40bb901
tphanson <tphanson@kambria.io>
2020-07-07 16:26:50 +0700

    nav camera, wheels

--- env/objs.py
+++ env/objs.py
@@ -1,12 +1,19 @@
 import pybullet as p
+import numpy as np
 
 
 def plane():
     return p.loadURDF('plane.urdf')
 
 
-def floor():
-    return p.loadURDF('env/model/floor.urdf')
+def floor(texture=False, wall=False):
+    floorId = p.loadURDF('env/model/floor.urdf')
+    if texture:
+        textureId = p.loadTexture('env/model/texture/xwood1.jpg')
+        p.changeVisualShape(floorId, -1, textureUniqueId=textureId)
+    if wall:
+        p.loadURDF('samurai.urdf')
+    return floorId
 
 
 def car():
@@ -14,4 +21,33 @@ def car():
 
 
 def ohmni():
-    return p.loadURDF('env/model/ohmni.urdf', [0, 0, 0.1])
+    start_pos = [0, 0, 0.1]
+    start_orientation = p.getQuaternionFromEuler([0, 0, 0])
+    ohmniId = p.loadURDF('env/model/ohmni.urdf', start_pos, start_orientation)
+
+    projection_matrix = p.computeProjectionMatrixFOV(
+        fov=80.0, aspect=1.0, nearVal=0.01, farVal=50)
+
+    def get_nav_image(img_shape=(96, 96)):
+        # Center of mass position and orientation (of tube)
+        tube_position, tube_orientation, _, _, _, _ = p.getLinkState(
+            ohmniId, 4, computeForwardKinematics=True)
+        tube_position = np.array(tube_position, dtype=np.float)
+        rotation = np.array(p.getMatrixFromQuaternion(
+            tube_orientation), dtype=np.float).reshape(3, 3)
+        # Initial vectors
+        init_eye_pos = np.array([0.015, 0, 0.4], dtype=np.float)
+        init_target_pos = np.array([0.1, 0, -1], dtype=np.float)
+        init_up_vector = np.array([0, 0, 1], dtype=np.float)
+        # Rotated vectors
+        eye_pos = tube_position + np.dot(rotation, init_eye_pos)
+        target_pos = eye_pos + np.dot(rotation, init_target_pos)
+        up_vector = np.dot(rotation, init_up_vector)
+        view_matrix = p.computeViewMatrix(
+            cameraEyePosition=eye_pos,
+            cameraTargetPosition=target_pos,
+            cameraUpVector=up_vector)
+        width, height = img_shape
+        return p.getCameraImage(width, height, view_matrix, projection_matrix)
+
+    return ohmniId, get_nav_image
--- env/test.py
+++ env/test.py
@@ -1,38 +1,36 @@
 import pybullet as p
 import pybullet_data
 
-from env.objs import floor, ohmni, plane
+from env.objs import floor, ohmni
+
+VELOCITY = 30
 
 
 def init_ws():
     clientId = p.connect(p.GUI)
     p.setGravity(0, 0, -10, physicsClientId=clientId)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
-    print(pybullet_data.getDataPath())
     return clientId
 
 
 def show():
-    clientId = init_ws()
+    init_ws()
     angle = p.addUserDebugParameter('Steering', -0.5, 0.5, 0)
-    throttle = p.addUserDebugParameter('Throttle', 0, 20, 0)
-    planeId = floor()
-    # carId = car()
-    ohmniId = ohmni()
+    throttle = p.addUserDebugParameter('Throttle', 0, 1, 0)
+    floor(texture=True, wall=True)
+    ohmniId, get_image = ohmni()
 
-    # wheel_indices = [1, 3, 4, 5]
-    # hinge_indices = [0, 2]
-    # while True:
-    #     user_angle = p.readUserDebugParameter(angle)
-    #     user_throttle = p.readUserDebugParameter(throttle)
-    #     for joint_index in wheel_indices:
-    #         p.setJointMotorControl2(carId, joint_index,
-    #                                 p.VELOCITY_CONTROL,
-    #                                 targetVelocity=user_throttle)
-    #     for joint_index in hinge_indices:
-    #         p.setJointMotorControl2(carId, joint_index,
-    #                                 p.POSITION_CONTROL,
-    #                                 targetPosition=user_angle)
-    #     p.stepSimulation()
     while True:
+        get_image((224, 224))
+        user_angle = p.readUserDebugParameter(angle)
+        user_throttle = p.readUserDebugParameter(throttle)
+        left_wheel = VELOCITY*(user_throttle+user_angle)
+        right_wheel = VELOCITY*(user_throttle-user_angle)
+        print(left_wheel, right_wheel)
+        p.setJointMotorControl2(ohmniId, 0,
+                                p.VELOCITY_CONTROL,
+                                targetVelocity=left_wheel)
+        p.setJointMotorControl2(ohmniId, 1,
+                                p.VELOCITY_CONTROL,
+                                targetVelocity=right_wheel)
         p.stepSimulation()
