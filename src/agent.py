913d30a65f9b8d036c5c7d1553a81d7187d2f5fe
tphanson <tphanson@kambria.io>
2020-10-29 11:10:30 +0700

    write qnetwork by hands

--- src/agent.py
+++ src/agent.py
@@ -1,91 +1,15 @@
-from abc import ABC, abstractmethod
 import tensorflow as tf
 from tensorflow import keras
-from tf_agents.agents import dqn, reinforce
-from tf_agents.networks import q_network, actor_distribution_network
-from tf_agents.utils import common
-from tf_agents.policies import policy_saver
-from tf_agents.experimental.train.utils import strategy_utils
 
 
-class Agent(ABC):
-    def __init__(self, env):
-        self.env = env
-        self.strategy = strategy_utils.get_strategy(
-            tpu=False,
-            use_gpu=(len(tf.config.list_physical_devices('GPU')) > 0)
-        )
-        self._discount = 0.9
-        self.global_step = tf.compat.v1.train.get_or_create_global_step()
-        self.checkpointer = None
-
-    def _checkpoint(self, checkpoint_dir, agent, replay_buffer):
-        if self.checkpointer is None:
-            self.checkpointer = common.Checkpointer(
-                ckpt_dir=checkpoint_dir,
-                max_to_keep=1,
-                agent=agent,
-                policy=agent.policy,
-                replay_buffer=replay_buffer,
-                global_step=self.global_step
-            )
-        return self.checkpointer
-
-    def save_checkpoint(self, checkpoint_dir, agent, replay_buffer):
-        checkpointer = self._checkpoint(checkpoint_dir, agent, replay_buffer)
-        checkpointer.save(self.global_step)
-
-    def load_checkpoint(self, checkpoint_dir, agent, replay_buffer):
-        checkpointer = self._checkpoint(checkpoint_dir, agent, replay_buffer)
-        checkpointer.initialize_or_restore()
-        self.global_step = tf.compat.v1.train.get_global_step()
-
-    @staticmethod
-    def save_policy(policy, saving_dir):
-        saver = policy_saver.PolicySaver(policy)
-        saver.save(saving_dir)
-
-    @staticmethod
-    def load_policy(saving_dir):
-        policy = tf.compat.v2.saved_model.load(saving_dir)
-        return policy
-
-    @abstractmethod
-    def gen_agent(self, train_step_counter):
-        """ Generate an agent instance """
-        return
-
-
-class REINFORCE(Agent):
-    def __init__(self, env):
-        super().__init__(env)
-        self.net = actor_distribution_network.ActorDistributionNetwork(
-            self.env.observation_spec(),
-            self.env.action_spec(),
-            conv_layer_params=[(32, 5, 1), (64, 5, 2),
-                               (128, 5, 2), (256, 5, 2)],
-            fc_layer_params=(64, 2))
-        self.optimizer = tf.compat.v1.train.AdamOptimizer()
-
-    def gen_agent(self, train_step_counter):
-        with self.strategy.scope():
-            agent = reinforce.reinforce_agent.ReinforceAgent(
-                self.env.time_step_spec(),
-                self.env.action_spec(),
-                actor_network=self.net,
-                optimizer=self.optimizer,
-                normalize_returns=True,
-                train_step_counter=train_step_counter)
-            agent.initialize()
-            return agent
-
-
-class DQN(Agent):
-    def __init__(self, env):
-        super().__init__(env)
-        self.preprocessing_layers = keras.models.Sequential([  # (96, 96, *)
+class DQN():
+    def __init__(self, env, train_step_counter):
+        self.train_step_counter = train_step_counter
+        self.collect_data_spec = self._define_collect_data_spec(env)
+        self.model = keras.models.Sequential([  # (96, 96, *)
             keras.layers.Conv2D(  # (92, 92, 16)
-                filters=16, kernel_size=(5, 5), strides=(1, 1), activation='relu'),
+                filters=16, kernel_size=(5, 5), strides=(1, 1), activation='relu',
+                input_shape=(96, 96, 4)),
             keras.layers.MaxPooling2D((2, 2)),  # (46, 46, 16)
             keras.layers.Conv2D(  # (42, 42, 32)
                 filters=32, kernel_size=(5, 5), strides=(1, 1), activation='relu'),
@@ -95,26 +19,20 @@ class DQN(Agent):
             keras.layers.MaxPooling2D((2, 2)),  # (5, 5, 64)
             keras.layers.Flatten(),
             keras.layers.Dense(64, activation='relu'),
+            keras.layers.Dense(32, activation='relu'),
+            keras.layers.Dense(5, activation='relu'),
         ])
-        self.net = q_network.QNetwork(
-            input_tensor_spec=self.env.observation_spec(),
-            action_spec=self.env.action_spec(),
-            # preprocessing_layers=self.preprocessing_layers,
-            conv_layer_params=[(64, 8, 2), (128, 5, 2), (256, 3, 2)],
-            fc_layer_params=(64, 32)
-        )
-        self.optimizer = tf.compat.v1.train.AdamOptimizer()
+        self.optimizer = keras.optimizers.Adam()
+    
+    def _define_collect_data_spec(self, env):
+        print(env.action_spec)
+        print(env.observation_spec)
+        return None
 
-    def gen_agent(self, train_step_counter):
-        with self.strategy.scope():
-            agent = dqn.dqn_agent.DqnAgent(
-                self.env.time_step_spec(),
-                self.env.action_spec(),
-                q_network=self.net,
-                optimizer=self.optimizer,
-                gamma=self._discount,
-                td_errors_loss_fn=common.element_wise_squared_loss,
-                train_step_counter=train_step_counter
-            )
-            agent.initialize()
-            return agent
+
+    @tf.function
+    def train_step(self, ds):
+        self.train_step_counter += 1
+
+    def train(self, ds):
+        pass
--- src/train.py
+++ src/train.py
@@ -1,7 +1,6 @@
 import os
 import time
 import tensorflow as tf
-from tf_agents.utils import common
 
 from env import OhmniInSpace
 from src.agent import DQN
@@ -17,7 +16,7 @@ checkpoint_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 tf.compat.v1.enable_v2_behavior()
 
 # Trick
-# No GPU: my super-extra-fast-and-furiuos-huhu machine
+# No GPU: my super-extra-fast-and-furiuos-ahuhu machine
 # GPUs: tranning servers
 LOCAL = not len(tf.config.list_physical_devices('GPU')) > 0
 
@@ -28,9 +27,8 @@ def check_point():
     train_env = tfenv.gen_env(gui=LOCAL)
 
     # Agent
-    algo = DQN(train_env)
     train_step_counter = tf.Variable(0)
-    agent = algo.gen_agent(train_step_counter)
+    agent = DQN(train_step_counter)
 
     # Replay buffer
     replay_buffer = ReplayBuffer(
@@ -53,47 +51,45 @@ def train():
     eval_env = tfenv.gen_env()
 
     # Agent
-    algo = DQN(train_env)
     train_step_counter = tf.Variable(0)
-    agent = algo.gen_agent(train_step_counter)
-    algo.net.summary()
+    agent = DQN(train_env, train_step_counter)
 
-    # Replay buffer
-    replay_buffer = ReplayBuffer(
-        agent.collect_data_spec,
-        batch_size=train_env.batch_size,
-    )
-    dataset = replay_buffer.get_pipeline()
+    # # Replay buffer
+    # replay_buffer = ReplayBuffer(
+    #     agent.collect_data_spec,
+    #     batch_size=train_env.batch_size,
+    # )
+    # dataset = replay_buffer.get_pipeline()
 
-    # Metrics and Evaluation
-    criterion = ExpectedReturn()
+    # # Metrics and Evaluation
+    # criterion = ExpectedReturn()
 
-    # Train
-    agent.train = common.function(agent.train)
-    agent.train_step_counter.assign(0)
+    # # Train
+    # agent.train = common.function(agent.train)
+    # agent.train_step_counter.assign(0)
 
-    num_iterations = 500000
-    eval_step = 1000
-    algo.load_checkpoint(checkpoint_dir, agent, replay_buffer.buffer)
-    start = time.time()
-    for _ in range(num_iterations):
-        replay_buffer.collect_step(train_env, agent.collect_policy)
-        experience, _ = next(dataset)
-        train_loss = agent.train(experience)
-        step = agent.train_step_counter.numpy()
-        # Evaluation
-        if step % eval_step == 0:
-            # Checkpoints
-            algo.save_checkpoint(checkpoint_dir, agent, replay_buffer.buffer)
-            avg_return = criterion.eval(eval_env, agent.policy)
-            print('Step = {0}: Average Return = {1}'.format(step, avg_return))
-            end = time.time()
-            print('Step estimated time: {:.4f}'.format((end-start)/eval_step))
-            start = time.time()
+    # num_iterations = 500000
+    # eval_step = 1000
+    # algo.load_checkpoint(checkpoint_dir, agent, replay_buffer.buffer)
+    # start = time.time()
+    # for _ in range(num_iterations):
+    #     replay_buffer.collect_step(train_env, agent.collect_policy)
+    #     experience, _ = next(dataset)
+    #     train_loss = agent.train(experience)
+    #     step = agent.train_step_counter.numpy()
+    #     # Evaluation
+    #     if step % eval_step == 0:
+    #         # Checkpoints
+    #         algo.save_checkpoint(checkpoint_dir, agent, replay_buffer.buffer)
+    #         avg_return = criterion.eval(eval_env, agent.policy)
+    #         print('Step = {0}: Average Return = {1}'.format(step, avg_return))
+    #         end = time.time()
+    #         print('Step estimated time: {:.4f}'.format((end-start)/eval_step))
+    #         start = time.time()
 
-    # Visualization
-    criterion.save()
-    DQN.save_policy(agent.policy, saving_dir)
+    # # Visualization
+    # criterion.save()
+    # DQN.save_policy(agent.policy, saving_dir)
 
 
 def run():
