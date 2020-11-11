48c8b566a9e921837e2647e8fcaeba777a794220
tphanson <tphanson@kambria.io>
2020-11-11 09:12:48 +0700

    bugfix

--- src/agent.py
+++ src/agent.py
@@ -23,22 +23,30 @@ class DQN():
         # Model
         self.policy = keras.Sequential([
             keras.Input(shape=env.observation_spec().shape),  # (96, 96, *)
+
             keras.layers.Conv2D(  # (92, 92, 16)
-                filters=16, kernel_size=(5, 5), strides=(1, 1), activation='relu'),
-            keras.layers.MaxPooling2D((2, 2), name='conv1'),  # (46, 46, 16)
+                filters=16, kernel_size=(5, 5), strides=(1, 1)),
+            keras.layers.BatchNormalization(),
+            keras.layers.ReLU(),
+            keras.layers.AveragePooling2D((2, 2), name='conv1'),  # (46, 46, 16)
+
             keras.layers.Conv2D(  # (42, 42, 32)
-                filters=32, kernel_size=(5, 5), strides=(1, 1), activation='relu'),
-            keras.layers.MaxPooling2D((2, 2), name='conv2'),  # (21, 21, 32)
+                filters=32, kernel_size=(5, 5), strides=(1, 1)),
+            keras.layers.BatchNormalization(),
+            keras.layers.ReLU(),
+            keras.layers.AveragePooling2D((2, 2), name='conv2'),  # (21, 21, 32)
+
             keras.layers.Conv2D(  # (10, 10, 64)
-                filters=64, kernel_size=(3, 3), strides=(2, 2), activation='relu'),
-            keras.layers.MaxPooling2D((2, 2), name='conv3'),  # (5, 5, 64)
+                filters=64, kernel_size=(3, 3), strides=(2, 2)),
+            keras.layers.BatchNormalization(),
+            keras.layers.ReLU(),
+            keras.layers.AveragePooling2D((2, 2), name='conv3'),  # (5, 5, 64)
+
             keras.layers.Flatten(),
             keras.layers.Dense(192, activation='relu', name='attention_layer'),
             keras.layers.Dense(self._num_actions, name='action_layer'),
         ])
-        self.optimizer = keras.optimizers.RMSprop(
-            learning_rate=0.00025, rho=0.95, epsilon=0.01
-        )
+        self.optimizer = keras.optimizers.Adam()
         # Setup checkpoints
         self.checkpoint = tf.train.Checkpoint(
             optimizer=self.optimizer,
