8ef848a3491336551fd95d56f112cfd08c0b3eb7
tphanson <tphanson@kambria.io>
2020-11-30 09:33:17 +0700

    ppo

--- src/agent.py
+++ src/agent.py
@@ -1,8 +1,7 @@
 import tensorflow as tf
-from tf_agents.agents import dqn
-from tf_agents.networks import q_network
+from tf_agents.agents import ppo
+from tf_agents.networks import actor_distribution_network, value_network
 from tf_agents.utils import common
-from tf_agents.policies import policy_saver
 
 
 class DQN():
@@ -10,18 +9,25 @@ class DQN():
         # Env
         self.env = env
         # Policy
-        self.net = q_network.QNetwork(
+        self.actor = actor_distribution_network.ActorDistributionNetwork(
+            self.env.observation_spec(),
+            self.env.action_spec(),
+            fc_layer_params=(128,)
+        )
+        self.critic = value_network.ValueNetwork(
             self.env.observation_spec(),
             self.env.action_spec(),
             fc_layer_params=(128,)
         )
         # Agent
         self.global_step = tf.compat.v1.train.get_or_create_global_step()
-        self.optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.000025)
-        self.agent = dqn.dqn_agent.DqnAgent(
+        self.optimizer = tf.compat.v1.train.AdamOptimizer(
+            learning_rate=0.000025)
+        self.agent = ppo.ppo_clip_agent.PPOClipAgent(
             self.env.time_step_spec(),
             self.env.action_spec(),
-            q_network=self.net,
+            actor_net=self.actor,
+            value_net=self.critic,
             optimizer=self.optimizer,
             td_errors_loss_fn=common.element_wise_squared_loss,
             train_step_counter=self.global_step)
@@ -42,13 +48,3 @@ class DQN():
     def load_checkpoint(self):
         self.checkpointer.initialize_or_restore()
         self.global_step = tf.compat.v1.train.get_global_step()
-
-    @staticmethod
-    def save_policy(policy, saving_dir):
-        saver = policy_saver.PolicySaver(policy)
-        saver.save(saving_dir)
-
-    @staticmethod
-    def load_policy(saving_dir):
-        policy = tf.compat.v2.saved_model.load(saving_dir)
-        return policy
